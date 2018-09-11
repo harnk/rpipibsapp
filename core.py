41a88f466ef9c2f98168987651af0af301b8573c
Scott Null <scott.null@gmail.com>
2018-09-11 12:19:01 -0400

    Str to json for processMessage

--- core.py
+++ core.py
@@ -177,7 +177,7 @@ class Node(object):
                     not self.background.msg_queue.empty():
                 msg = self.background.msg_queue.get()
                 print "from msg queue: ",msg
-                self.processMessage(msg)
+                self.processMessage(json.dumps(msg))
 
 
 
