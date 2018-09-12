146be636905dbe4c60d7293cd48d2b733cbd0e80
Scott Null <scott.null@gmail.com>
2018-09-12 08:52:47 -0400

    correct logfile name

--- installation/EnableGPSHat.sh
+++ installation/EnableGPSHat.sh
@@ -30,12 +30,12 @@ if sudo grep -i -q "splash" /boot/cmdline.txt; then
 	# check if plymouth.ignore-serial-consoles is there
 	if sudo grep -i -q "plymouth.ignore-serial-consoles" /boot/cmdline.txt; then
 		# you're good!
-		echo "Splash screen already told to ignore serial console." | tee -a $LOGPATH/RH_install_log.txt
+		echo "Splash screen already told to ignore serial console." | tee -a $LOGPATH/PC_install_log.txt
 	else
 		# add command to ignore serial console
 		sudo echo "plymouth.ignore-serial-consoles" >> /boot/cmdline.txt
-		echo "Updated splash screen to ignore serial console." | tee -a $LOGPATH/RH_install_log.txt
+		echo "Updated splash screen to ignore serial console." | tee -a $LOGPATH/PC_install_log.txt
 	fi
 else
-	echo "No splash screen is used, so no updates needed." | tee -a $LOGPATH/RH_install_log.txt
+	echo "No splash screen is used, so no updates needed." | tee -a $LOGPATH/PC_install_log.txt
 fi
