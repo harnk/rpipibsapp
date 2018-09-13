492a624c44c7e422f1c2dd699e7fff9f58aad001
Scott Null <scott.null@gmail.com>
2018-09-13 16:37:08 -0400

    compare received location in message(args[key_current_position] with my current location

--- tasks/pibs.py
+++ tasks/pibs.py
@@ -5,6 +5,7 @@ import tools
 import os
 import subprocess
 import platform
+import gps
 from definitions import *
 
 current_milli_time = lambda: int(round(time.time() * 1000))
@@ -12,6 +13,7 @@ TAG = 'tasks.pibs.'
 
 def detectAndAvoid(node,args):
     print 'TASK: ' + TAG + sys._getframe().f_code.co_name + ' @time:', current_milli_time(), 'ms'
+    gps = node.sensors.getAnySensor('sensors.locationing')
     print args[key_source_id]
     print args[key_uav_class]
     print args[key_current_position]
@@ -19,6 +21,9 @@ def detectAndAvoid(node,args):
     print 'Detect and Avoid Logic Goes Here TBD'
     # dist = tools.getDistanceInMeters(args[key_current_position],[41.739565, -86.099060, 100])
     # dist = tools.getDistanceInMeters([41.739565, -86.099060, 100],[41.739001, -86.097830, 110])
-    dist = tools.getDistanceInMeters([41.739565, -86.099060, 100],[41.739565, -86.099060, 500])
+    # dist = tools.getDistanceInMeters([41.739565, -86.099060, 100],[41.739565, -86.099060, 500])
+
+    # compare received location in message(args[key_current_position] with my current location
+    dist = tools.getDistanceInMeters(args[key_current_position],[gps.getlocation()])
     print str(dist) + " meters away factoring in alt ..."
 
